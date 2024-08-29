fe516e8e98e95f1aad5de8c35014bfe302ce31b0
Michael Gerullis <mgerullis@pm.me>
2024-08-29 20:02:44 +0200

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import HomeView from "@/views/home";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Fast & Secure URL Shortener | Shorten Your Links Instantly | No Bullshit, no ads, just short links.",
+  description:
+    "Create short, custom URLs quickly and easily with our free URL shortener. No Bullshit, no ads, just short links.",
+  keywords:
+    "URL shortener, link shortener, custom URLs, link tracking, marketing tools.  No Bullshit, no ads, just short links.",
+  openGraph: {
+    title: "Fast & Secure URL Shortener | Shorten Your Links Instantly | No Bullshit, no ads, just short links.",
+    description:
+      "Create short, custom URLs quickly and easily with our free URL shortener. No Bullshit, no ads, just short links.",
+    type: "website",
+    url: "https://your-domain.com",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fast & Secure URL Shortener | Shorten Your Links Instantly | No Bullshit, no ads, just short links.",
+    description:
+      "Create short, custom URLs quickly and easily with our free URL shortener. No Bullshit, no ads, just short links.",
+  },
+};
 
 export default function Home() {
-
-  return (
-    <HomeView />
-  );
+  return <HomeView />;
 }
