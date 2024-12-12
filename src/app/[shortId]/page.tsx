b7136247a0608518d0894e162b53d10737fb0d33
Michael Gerullis <mgerullis@pm.me>
2024-12-13 00:58:37 +0100

    cleanup

--- src/app/[shortId]/page.tsx
+++ src/app/[shortId]/page.tsx
@@ -1,12 +1,54 @@
-import { kv } from "@vercel/kv";
-import { redirect, notFound } from "next/navigation";
+"use client";
+import { useEffect } from "react";
+import { useState } from "react";
 
-export default async function RedirectPage({
+const getUrl = async (token: string, shortId: string) => {
+  const res = await fetch("/api/get-urls", {
+    method: "POST",
+    body: JSON.stringify({ token }),
+  });
+  return (await res.json()).urls.find(
+    (url: { shortId: string }) => url.shortId === shortId
+  );
+};
+
+export default function RedirectPage({
   params,
 }: {
   params: { shortId: string };
 }) {
   const { shortId } = params;
-  const url = await kv.get<string>(shortId);
-  return url ? redirect(url) : notFound();
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const storedToken = localStorage.getItem("shortener_token");
+        if (!storedToken) {
+          setError("No token found");
+          return;
+        }
+        const url = await getUrl(storedToken, shortId);
+        if (url) {
+          window.location.href = url.url;
+        } else {
+          setError("URL not found");
+        }
+      } catch (e) {
+        setError("Error getting token");
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {!loading && !error && <p>Redirecting...</p>}
+      {error && <p>{error}</p>}
+    </>
+  );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,12 +2,17 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h2 className="text-2xl font-bold mb-4">Not Found</h2>
-      <p className="mb-4">Could not find the requested resource</p>
-      <Link href="/" className="text-blue-500 hover:underline">
-        Return Home
-      </Link>
-    </div>
+    <>
+      <h1 className="text-purple-600 text-2xl mt-24">Not Found</h1>
+      <div className="bg-black rounded-lg shadow-2xl max-w-md w-full text-center mt-6 border-2 border-purple-600 p-6">
+        <p className="text-purple-600 mb-6">Could not find the requested resource</p>
+        <Link
+          href="/"
+          className="text-purple-600 ring-1 ring-purple-500 font-medium py-2 px-4 rounded-md shadow transition duration-150"
+        >
+          Return Home
+        </Link>
+      </div>
+    </>
   );
 }
