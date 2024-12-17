41b86127e1969931671daf929252ba969bcbbfe3
Michael Gerullis <mgerullis@pm.me>
2024-12-17 14:29:00 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+if (process.env.NEXT_OUTPUT) {
+  nextConfig.output = process.env.NEXT_OUTPUT;
+}
 
 export default nextConfig;
